1f0a3201caeac970bb33468257a00530d9462b4d
tomori_aki <tomori.aki2961@gmail.com>
2018-12-11 02:42:52 +0900

    Unityプロジェクトの追加

---
+++ find-references-in-scene/Assets/FindReferencesInScene/Editor/FindReferencesInScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System;
+using System.Reflection;
+
+namespace SIN.Editor
+{
+    public class FindReferencesInScene
+    {
+        [MenuItem("CONTEXT/Component/Find References In Scene")]
+        private static void _FindReferencesInScene(MenuCommand command)
+        {
+            SetSearchFilter(string.Format("ref:{0}:", command.context.GetInstanceID()));
+        }
+
+        private static void SetSearchFilter(
+            string searchString,
+            SearchableEditorWindow.SearchMode searchMode = SearchableEditorWindow.SearchMode.All,
+            bool setAll = false)
+        {
+            var sceneHierarchyWindowType = Type.GetType("UnityEditor.SceneHierarchyWindow,UnityEditor");
+            var window = EditorWindow.GetWindow(sceneHierarchyWindowType);
+            var setSearchMethodInfo = typeof(SearchableEditorWindow).GetMethod("SetSearchFilter", BindingFlags.NonPublic | BindingFlags.Instance);
+            var parameters = new object[] { searchString, searchMode, setAll };
+            setSearchMethodInfo.Invoke(window, parameters);
+        }
+    }
+}
